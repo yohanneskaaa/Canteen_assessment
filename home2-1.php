2d6a7d5659b3af9bcdc7d53dea41b98396b68474
yohanneskaaa <90356155+yohanneskaaa@users.noreply.github.com>
2021-09-19 17:42:42 +1200

    Home page 1st version of 2nd sprint
    Simple applied CSS

---
+++ home2-1.php
@@ -0,0 +1,81 @@
+<?php
+$con = mysqli_connect("localhost", "yohanneska", "wildfog34", "yohanneska_canteen" );
+if(mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL:".mysqli_connect_error(); die();}
+else{
+    echo "connected to database";
+}
+
+$all_specials_query = "SELECT Weekday FROM specials";
+$all_specials_result = mysqli_query($con, $all_specials_query);
+if(isset($_GET['special'])){
+    $id = $_GET['special'];
+}else{
+    $id = "Monday";
+}
+/* Drinks Query*/
+/*SELECT DrinkID, DrinkName FROM Drinks*/
+$this_specials_query = "SELECT specials.weekday, specials.FoodID, specials.DrinkID, specials.TreatID, Foods.FItem, Drinks.DItem,
+Foods.FoodID, Drinks.DrinkID, Treats.TreatID, Treats.TItem From specials, foods, drinks, treats WHERE specials.DrinkID = drinks.DrinkID
+AND specials.FoodID = Foods.FoodID AND Weekday = '" . $id . "'";
+$this_specials_result = mysqli_query($con, $this_specials_query);
+$this_specials_record = mysqli_fetch_assoc($this_specials_result);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>CANTEEN</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style2-1.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Kalam&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Concert+One&display=swap" rel="stylesheet">
+</head>
+<body>
+<header>
+    <h1>WGC CANTEEN</h1>
+    <nav>
+        <ul>
+            <li><a href="home2-1.php" >HOME</a></li>
+            <li><a href="food2-1.php" >FOOD</a></li>
+            <li><a href="drinks2-1.php" >DRINKS</a></li>
+            <li><a href="treats2-1.php" >TREATS</a></li>
+        </ul>
+    </nav>
+</header>
+<hr>
+</body>
+
+<main>
+    <h2>Weekly Specials:</h2>
+
+    <!--specials form-->
+    <form name='specials_form' id='specials_form' method = 'get' action ='home2-1.php'>
+        <select id = 'special' name = 'special'>
+            <!--options-->
+            <?php
+            while($all_specials_record = mysqli_fetch_assoc($all_specials_result)){
+                echo "<option value = '". $all_specials_record['Weekday'] . "'>";
+                echo $all_specials_record['Weekday'];
+                echo "</option>";
+            }
+            ?>
+        </select>
+        <input type='submit' name='specials_button' value='show me the specials information'>
+    </form>
+
+    <?php
+    echo "<p> DAY: " . $this_specials_record['weekday'] . "<br>";
+    echo "<p> FOOD: " . $this_specials_record['FItem'] . "<br>";
+    echo "<p> DRINK: " . $this_specials_record['DItem'] . "<br>";
+    echo "<p> TREAT: " . $this_specials_record['TItem'] . "<br>";
+    ?>
+
+    <p> All items on special for the day are 20% </p>
+
+</main>
+</html>
